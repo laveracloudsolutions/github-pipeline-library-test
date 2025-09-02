cb75b624e2259bf3d8a4fecf7167c441bf97a12f
CHAMPALAUNE Maël <mael.champalaune@orange.com>
2025-09-02 10:49:54 +0200

    http-get-response-time.sh

--- .scripts/http-get-response-time.sh
+++ .scripts/http-get-response-time.sh
@@ -6,28 +6,27 @@
 URL="http://100.100.58.149/api/v1/healthcheck"
 
 # Nombre d'appels
-NOMBRE_APPELS=200
+NOMBRE_APPELS=20
 # Initialisation des variables
 total_time=0
 max_time=0
+echo "Test de l'URL: $URL"
 
 for ((i=1; i<=NOMBRE_APPELS; i++))
 do
-  # Mesurer le temps de réponse en secondes
+  # Obtenir le temps de réponse
   response_time=$(curl -s -o /dev/null -w "%{time_total}" "$URL")
+  echo "response_time : $response_time"
 
-  # Ajouter le temps à la somme
-  total_time=$(echo "$total_time + $response_time" | awk '{printf "%.6f", $0 + $1}')
-
-  # Vérifier si c'est le maximum
-  comparison=$(echo "$response_time > $max_time" | awk '{print ($0 ? 1 : 0)}')
-  if [ "$comparison" -eq 1 ]; then
-    max_time=$response_time
-  fi
+  # Accumuler le total et le maximum avec awk
+  temps_total=$(awk -v rt="$response_time" -v total="$temps_total" 'BEGIN {printf "%.6f", total + rt}')
+  temps_max=$(awk -v rt="$response_time" -v max="$temps_max" 'BEGIN {print (rt > max) ? rt : max}')
 done
 
-# Calcul du temps moyen
-moyenne=$(echo "$total_time / $NOMBRE_APPELS" | awk '{printf "%.3f", $0}')
+# Calculer la moyenne
+moyenne=$(awk -v total="$temps_total" -v count="$NOMBRE_APPELS" 'BEGIN {printf "%.3f", total / count}')
 
 echo "Temps de réponse moyen après $NOMBRE_APPELS appels : $moyenne secondes"
-echo "Temps de réponse maximum : $max_time secondes"
+echo "Temps de réponse maximum : $temps_max secondes"
+
+
