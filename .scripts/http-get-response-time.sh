721e5e568aad26c497be9a4525bd792d3487af39
CHAMPALAUNE Maël <mael.champalaune@orange.com>
2025-09-03 09:08:02 +0200

    update .scripts/http-get-response-time.sh

--- .scripts/http-get-response-time.sh
+++ .scripts/http-get-response-time.sh
@@ -3,7 +3,13 @@
 
 # URL à tester
 #URL="https://app1-int.petrolavera.com/api/v1/healthcheck"
-URL="http://100.100.58.149/api/v1/healthcheck"
+# URL="http://100.100.58.149/api/v1/healthcheck"
+
+if [[ $# -lt 1 ]] ; then
+    echo "Usage: $0 <site-url>"
+    exit 1
+fi
+URL=$1
 
 # Nombre d'appels
 NOMBRE_APPELS=20
@@ -28,5 +34,3 @@ moyenne=$(awk -v total="$temps_total" -v count="$NOMBRE_APPELS" 'BEGIN {printf "
 
 echo "Temps de réponse moyen après $NOMBRE_APPELS appels : $moyenne secondes"
 echo "Temps de réponse maximum : $temps_max secondes"
-
-
